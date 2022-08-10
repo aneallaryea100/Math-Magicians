b262d3aaa98941b9a71010740b9e07c9c79b476b
Aneal Laryea <aneallaryea100@gmail.com>
2022-08-10 17:05:30 +0000

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
 import './App.css';
 import Calculator from './component/calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
