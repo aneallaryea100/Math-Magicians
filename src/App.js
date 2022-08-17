5d9e34d745869fc0ca351501bcf3c6dcb8e7f81a
Aneal Laryea <aneallaryea100@gmail.com>
2022-08-17 16:21:40 +0000

    routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './component/calculator';
+import NavBar from './component/NavBar';
+import Home from './component/Home';
+import Qoutes from './component/Qoutes';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Calculator />
-    );
-  }
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Qoutes" element={<Qoutes />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="containerHold">
+      <h1 className="titleHead">WELCOME TO OUR PAGE!</h1>
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum
+      </p>
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum
+      </p>
+    </div>
+  );
+}
+
+export default Home;
