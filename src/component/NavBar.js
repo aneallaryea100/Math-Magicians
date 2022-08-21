b4d1d31299d024c4e9d25f8c348a6c2dbdc166e2
Aneal Laryea <aneallaryea100@gmail.com>
2022-08-21 10:24:14 +0000

    fix testing error

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,17 +1,18 @@
-/* eslint-disable react/function-component-definition */
 import React from 'react';
 import './navbar.css';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => (
-  <div className="navholder">
-    <h1 className="headText">Math Magicians</h1>
-    <ul className="ulTag">
-      <NavLink to="/" className="nav">Home</NavLink>
-      <NavLink to="/calculator" className="nav">Calculator</NavLink>
-      <NavLink to="/Qoutes" className="nav">Qoutes</NavLink>
-    </ul>
-  </div>
-);
+function NavBar() {
+  return (
+    <div className="navholder">
+      <h1 className="headText">Math Magicians</h1>
+      <ul className="ulTag">
+        <NavLink to="/" className="nav">Home</NavLink>
+        <NavLink to="/calculator" className="nav">Calculator</NavLink>
+        <NavLink to="/Qoutes" className="nav">Qoutes</NavLink>
+      </ul>
+    </div>
+  );
+}
 
 export default NavBar;
